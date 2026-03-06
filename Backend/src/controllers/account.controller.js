f30ac256962da4508685eb502967e1cf7692863f
Tauseef Ahmad <ertauseefahmad@gmail.com>
2026-03-06 22:40:34 +0530

    feat: add account module (account.model, account.controller, account.routes) with protected account creation API

--- Backend/src/controllers/account.controller.js
+++ Backend/src/controllers/account.controller.js
@@ -1,2 +1,16 @@
 const accountModel = require("../models/account.model")
 
+async function createAccountController(req, res) {
+    const account = await accountModel.create({
+        user: req.user._id
+    })
+
+    return res.status(201).json({
+        message: "Account created successfully",
+        account
+    })
+}
+
+module.exports = {
+    createAccountController
+}
--- Backend/src/routers/account.routes.js
+++ Backend/src/routers/account.routes.js
@@ -1,10 +1,16 @@
 const express = require("express")
-
+const authMiddleware = require("../middleware/auth.middleware")
+const accountController = require("../controllers/account.controller")
 
 
 
 const router = express.Router()
 
-
+/**
+ *  - Post /api/accounts/
+ * @desc create a new account
+ * - protected route, only accessible to authenticated users
+ */
+router.post("/", authMiddleware.authMiddleware, accountController.createAccountController)
 
 module.exports = router
